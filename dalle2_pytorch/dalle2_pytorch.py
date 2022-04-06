bcbaac4d328383b832cbb7588b5a5257413265be
rishabh anand <rish@rishabhs-MacBook-Pro.local>
2022-04-07 02:08:11 +0800

    Add initial files

---
+++ dalle2_pytorch/dalle2_pytorch.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class DALLE2(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        pass
